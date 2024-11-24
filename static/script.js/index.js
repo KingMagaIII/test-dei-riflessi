f819ca96ffc97e46433a76fec6f79c151bd9a371
KingMagaIII <andreamagaletti14@gmail.com>
2024-11-24 20:29:56 +0100

    semaforo

---
+++ static/script.js/index.js
@@ -0,0 +1,55 @@
+function random(min, max) {
+    let n=Math.random()*(max-min)+min
+    n=parseInt(n)
+    console.log("Random delay:",n)
+    return n
+}
+
+function cambioColore() {
+    inizio.innerHTML="CLICCA ORA!"
+    sinistra.classList.replace("rosso","verde")
+    centro.classList.replace("rosso","verde")
+    destra.classList.replace("rosso","verde")
+    pausa=true
+    startTimer=Date.now();
+}
+
+function restart() {
+    sinistra.classList.replace("verde","rosso")
+    centro.classList.replace("verde","rosso")
+    destra.classList.replace("verde","rosso")
+    pausa=false
+    inizio.disabled=false
+    inizio.innerHTML="INIZIA IL TEST"
+}
+
+let inizio=document.querySelector("#inizio")
+let sinistra=document.querySelector("#sinistra")
+let centro=document.querySelector("#centro")
+let destra=document.querySelector("#destra")
+let riflessi=document.querySelector("#riflessi")
+let tempo=document.querySelector("#tempo")
+let pausa=false
+let startTimer
+let tempoMigliore=Infinity
+
+inizio.addEventListener("click",function(){
+    if (!pausa){
+       
+        restart()
+        let delay=random(1000, 3000)
+        riflessi.innerHTML="Preparati..."
+        setTimeout(cambioColore,delay)
+    }else{
+        let tempoDiReazione=(Date.now()-startTimer)/1000
+        riflessi.innerHTML="Tempo di reazione: "+tempoDiReazione+" secondi"
+        if (tempoDiReazione<tempoMigliore){
+            tempoMigliore=tempoDiReazione
+            tempo.innerHTML="Record personale: "+tempoMigliore+" secondi"
+        } else {
+            tempo.innerHTML="Record personale: "+tempoMigliore+" secondi"
+        }
+        inizio.disabled=true
+        setTimeout(restart,3000)
+    }
+})
